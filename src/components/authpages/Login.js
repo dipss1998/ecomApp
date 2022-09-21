b121437bcbb4c7b393388bb8aa2d70a36e6108ab
dipss1998 <dipu.malakar@cbnits.com>
2022-09-21 12:30:05 +0530

    signup_21/09/22

--- src/components/authpages/Login.js
+++ src/components/authpages/Login.js
@@ -79,7 +79,7 @@ const Login = () => {
             // console.log("user login succesfully done");
 
 
-            const data = await fetch("http://localhost:5000/login",{
+            const data = await fetch("http://localhost:8000/login",{
                 method:"POST",
                 headers:{
                     "Content-Type":"application/json"
